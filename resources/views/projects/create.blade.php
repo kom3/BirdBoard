a17c3648493a0d2948deb4db7cd8727f72370bb9
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2019-05-23 23:00:07 +0700

    Completed Tutorial: 08-Prepping the Frontend

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,40 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Birdboard</title>
+@section('content')
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
-</head>
+<form method="POST" action="/projects">
+    @csrf
+    <h1 class="heading is-1">Create a project</h1>
+    <div class="field">
+        <label for="title" class="label">Title</label>
 
-<body>
-
-    <form method="POST" action="/projects" class="container" style="padding-top: 40px;">
-        @csrf
-        <h1 class="heading is-1">Create a project</h1>
-        <div class="field">
-            <label for="title" class="label">Title</label>
-
-            <div class="control">
-                <input type="text" class="input" name="title" placeholder="Title">
-            </div>
+        <div class="control">
+            <input type="text" class="input" name="title" placeholder="Title">
         </div>
-        <div class="field">
-            <label for="title" class="label">Description</label>
+    </div>
+    <div class="field">
+        <label for="title" class="label">Description</label>
 
-            <div class="control">
-                <textarea name="description" id="description" cols="30" rows="10" class="textarea"></textarea>
-            </div>
+        <div class="control">
+            <textarea name="description" id="description" cols="30" rows="10" class="textarea"></textarea>
         </div>
-        <div class="field">
-            <div class="control">
-                <button class="button is-link" type="submit">Create Project</button>
-            </div>
+    </div>
+    <div class="field">
+        <div class="control">
+            <button class="button is-link" type="submit">Create Project</button>
+            <a href="/projects">Cancel</a>
         </div>
-    </form>
-</body>
-
-</html>
+    </div>
+</form>
+@endsection
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Birdboard</title>
-</head>
-<body>
-    <h1>Birdboard</h1>
-    <ul>
-        @forelse ($projects as $project)
-            <li>
-                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-            </li>
-        @empty
-            <li>No Projects yet.</li>
-        @endforelse
-    </ul>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="flex items-center mb-3">
+    <h1 class="mr-auto">Birdboard</h1>
+    <a href="/projects/create">New Project</a>
+</div>
+<ul>
+    @forelse ($projects as $project)
+    <li>
+        <a href="{{ $project->path() }}">{{ $project->title }}</a>
+    </li>
+    @empty
+    <li>No Projects yet.</li>
+    @endforelse
+</ul>
+@endsection
