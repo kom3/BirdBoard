9fae5f9d9af8c36d92279180d7523646b561afce
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2019-05-23 22:25:54 +0700

    Completed Tutorial: 06-Scoping Projects

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -30,15 +30,29 @@ class ProjectsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_view_a_project()
+    public function a_user_can_view_their_project()
     {
+        $this->be(factory('App\User')->create());
+
         $this->withoutExceptionHandling();
 
-        $project = factory('App\Project')->create();
+        $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
 
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
+    }
+
+    /** @test */
+    public function an_authenticated_user_cannot_view_the_projects_of_others()
+    {
+        $this->be(factory('App\User')->create());
+
+        // $this->withoutExceptionHandling();
+
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertStatus(403);
     }
 
     /** @test */
@@ -62,10 +76,24 @@ class ProjectsTest extends TestCase
     }
 
     /** @test */
-    public function only_authenticated_user_can_create_project()
+    public function guest_cannot_not_create_project()
     {
         $attributes = factory('App\Project')->raw();
 
         $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+    /** @test */
+    public function guest_cannot_not_view_project()
+    {
+        $this->get('/projects')->assertRedirect('login');
+    }
+
+    /** @test */
+    public function guest_cannot_not_view_single_project()
+    {
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertRedirect('login');
     }
 }
