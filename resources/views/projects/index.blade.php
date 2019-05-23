db10a7687675b76cc7312010856c005e0eb40fab
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2019-05-23 23:32:31 +0700

    Completed Tutorial: 09-Go Go Gadget Tailwind

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="flex items-center mb-3">
-    <h1 class="mr-auto">Birdboard</h1>
-    <a href="/projects/create">New Project</a>
-</div>
-<ul>
-    @forelse ($projects as $project)
-    <li>
-        <a href="{{ $project->path() }}">{{ $project->title }}</a>
-    </li>
-    @empty
-    <li>No Projects yet.</li>
-    @endforelse
-</ul>
+    <div class="flex items-center mb-3">
+        <a href="/projects/create">New Project</a>
+    </div>
+
+    <div class="flex">
+        @forelse ($projects as $project)
+            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px;">
+                <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+
+                <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+            </div>
+        @empty
+            <div>No Projects yet.</div>
+        @endforelse
+    </div>
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    theme: {
+        extend: {
+            boxShadow: {
+                default: '0 0 5px 0 rgba(0, 0, 0, 0.08)'
+            },
+            colors: {
+                'grey-light': '#f5f6f9',
+                grey: 'rgba(0, 0, 0, 0.4)'
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
 }
