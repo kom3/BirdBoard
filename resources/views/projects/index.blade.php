51d053b7f79e80afb6ea30426a3e12a5b1a9638f
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2019-05-23 19:15:24 +0700

    Completed Tutorial: 04-Model Tests

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -7,10 +7,15 @@
     <title>Birdboard</title>
 </head>
 <body>
-    <li>
-        @foreach ($projects as $project)
-            <li>{{ $project->title }}</li>
-        @endforeach
-    </li>
+    <h1>Birdboard</h1>
+    <ul>
+        @forelse ($projects as $project)
+            <li>
+                <a href="{{ $project->path() }}">{{ $project->title }}</a>
+            </li>
+        @empty
+            <li>No Projects yet.</li>
+        @endforelse
+    </ul>
 </body>
 </html>
--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -10,36 +10,48 @@ class ProjectsTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
-   /** @test */
-   public function a_user_can_create_a_project()
-   {
-       $this->withoutExceptionHandling();
+    /** @test */
+    public function a_user_can_create_a_project()
+    {
+        $this->withoutExceptionHandling();
 
-       $attributes = [
+        $attributes = [
             'title' => $this->faker->sentence,
             'description' => $this->faker->paragraph
-       ];
+        ];
 
-       $this->post('/projects', $attributes)->assertRedirect('/projects');
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
 
-       $this->assertDatabaseHas('projects', $attributes);
+        $this->assertDatabaseHas('projects', $attributes);
 
-       $this->get('/projects')->assertSee($attributes['title']);
-   }
+        $this->get('/projects')->assertSee($attributes['title']);
+    }
 
-   /** @test */
-   public function a_project_requires_a_title()
-   {
-       $attributes = factory('App\Project')->raw(['title' => '']);
+    /** @test */
+    public function a_user_can_view_a_project()
+    {
+        $this->withoutExceptionHandling();
 
-       $this->post('/projects', $attributes)->assertSessionHasErrors('title');
-   }
+        $project = factory('App\Project')->create();
 
-   /** @test */
-   public function a_project_requires_a_description()
-   {
-       $attributes = factory('App\Project')->raw(['description' => '']);
+        $this->get($project->path())
+            ->assertSee($project->title)
+            ->assertSee($project->description);
+    }
 
-       $this->post('/projects', $attributes)->assertSessionHasErrors('description');
-   }
+    /** @test */
+    public function a_project_requires_a_title()
+    {
+        $attributes = factory('App\Project')->raw(['title' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_project_requires_a_description()
+    {
+        $attributes = factory('App\Project')->raw(['description' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
+    }
 }
